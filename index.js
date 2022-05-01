fcd68e5450623323ceca6d9761dd90791cb2237a
EmilBjorkeng <emil.bjorkeng@hotmail.com>
2022-05-01 02:27:02 +0200

    webhook

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const express = require('express');
+const bodyParser = require('body-parser')
 const app = express();
+app.use(bodyParser.json());
 
 app.get('/', function (req, res) {
    res.sendFile(__dirname + "/index.html");
+})
+
+app.post('/webhook', function(req, res) {
+    console.log(req.body);
+    res.sendStatus(200);
 })
 
 var server = app.listen(8080, function () {
